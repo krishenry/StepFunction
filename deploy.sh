849b6ef9e27593298b9ed05d19a1be707660d452
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-02 14:17:05 +0100

    deploy modified

--- deploy.sh
+++ deploy.sh
@@ -13,8 +13,9 @@ ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 #echo $ARN
 VAR="/StepFunction/deploy.sh" 
 #echo $VAR
+ROLE="arn:aws:iam::015887481462:role/AcunaStepFunctionRole"
+#echo $ROLE
 
 #local directory /c/Users/uc245836/Desktop/AWS/StepFunction/deploy.sh
 
-aws StepFunction update-state-machine --state-machine-arn $ARN --definition $VAR
-#--cli-input-json 
+aws StepFunction update-state-machine --state-machine-arn $ARN --definition $VAR --role-arn $ROLE
