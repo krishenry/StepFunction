5ffc7ea1a8ff71f65e5f50edb23f0dd7a61ec71c
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-03 15:44:51 +0100

    Added echo

--- test.sh
+++ test.sh
@@ -4,5 +4,6 @@ filename='lambdanames.txt'
 chmod +x deployAlias.sh
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	export line
-	./deployAlias.sh 
+	echo $line
+	./deployAlias.sh
 done < $filename
