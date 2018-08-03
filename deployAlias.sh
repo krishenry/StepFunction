f5843d0df33551b9f889328ed293aeec61978991
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-03 11:34:07 +0100

    Added variables

--- deployAlias.sh
+++ deployAlias.sh
@@ -9,6 +9,8 @@ trap deployFail ERR
 CURDIR=`pwd`
 #NAME=$(cat ./Code/lambdaName)
 NAME=LizzieTestDev
+ALIAS=KRIS
+VERSION=1
 #VERSION=$(cat ./Code/version)
 #BUILD_VERSION=$VERSION.$GO_PIPELINE_COUNTER
 #build_number=$1
@@ -16,8 +18,8 @@ NAME=LizzieTestDev
 existing_aliases=$(aws lambda list-aliases --function-name ${NAME} --region ${REGION} --output json| jq -r '.Aliases[] | {Name: .Name}')
 
 
-if [[ $existing_aliases == *"DEV"* ]]; then
-  aws lambda update-alias --region ${REGION} --function-name ${NAME} --description "${NAME}" --function-version 1  --name KRIS
+if [[ $existing_aliases == *"$ALIAS"* ]]; then
+  aws lambda update-alias --region ${REGION} --function-name ${NAME} --description "${NAME}" --function-version $VERSION  --name $ALIAS
 else
-   aws lambda create-alias --region ${REGION} --function-name ${NAME} --description "${NAME}" --function-version 1 --name KRIS
+   aws lambda create-alias --region ${REGION} --function-name ${NAME} --description "${NAME}" --function-version $VERSION --name $ALIAS
 fi
