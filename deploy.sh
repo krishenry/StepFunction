903dbdb7ffb79d5d0ea2abc1367cbd7084f99a90
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-03 10:13:30 +0100

    cat deploy

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ CURDIR=`pwd`
 
 ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 #echo $ARN
-VAR="deploy.sh" 
+VAR=`cat deploy.sh`
 #echo $VAR
 REGION="us-east-1"
 #local directory /c/Users/uc245836/Desktop/AWS/StepFunction/deploy.sh
