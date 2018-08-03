a78b3014ed0019d75fd4172277f38c361fc42694
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-03 10:09:44 +0100

    Removed arn

--- deploy.sh
+++ deploy.sh
@@ -11,11 +11,9 @@ CURDIR=`pwd`
 
 ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 #echo $ARN
-VAR="/StepFunction/deploy.sh" 
+VAR="deploy.sh" 
 #echo $VAR
-ROLE="arn:aws:iam::015887481462:role/AcunaStepFunctionRole"
-#echo $ROLE
 REGION="us-east-1"
 #local directory /c/Users/uc245836/Desktop/AWS/StepFunction/deploy.sh
 
-aws stepfunctions update-state-machine --state-machine-arn $ARN --definition $VAR --role-arn $ROLE --region $REGION
+aws stepfunctions update-state-machine --state-machine-arn $ARN --definition $VAR --region $REGION
