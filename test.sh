f9124f09d45c2d7fa9bd588b887738240b3577bc
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-03 14:49:12 +0100

    Aliasing through another script

--- test.sh
+++ test.sh
@@ -3,6 +3,5 @@
 filename='lambdanames.txt'
 while IFS='' read -r line || [[ -n "$line" ]]; do
 	export line
-	./deployAlias #./deployTest #$line
-
+	./deployAlias.sh #./deployTest #$line
 done < $filename
