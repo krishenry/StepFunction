ea06455ddaa758894fea4fb5e0e680097a56c49c
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-06 15:48:30 +0100

    Reset1

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -16,7 +16,7 @@ ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 #ARN=aws stepfunctions describe-step-function --name Kris-StepFunction || jq .arn
 #LizzieTestDev=aws lambda describe-lambda --name $from file || jq .arn
 
-VAR=`cat example.json`
+VAR=`cat exampleOriginal.json`
 
 aws stepfunctions update-state-machine --state-machine-arn $ARN --definition "$VAR" --region $REGION
 
