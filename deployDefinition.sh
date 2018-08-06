57e071263c56f0a6bdb0dc64cc007dd6a4aaf07e
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-06 11:46:14 +0100

    Definition Changes

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -11,14 +11,27 @@ CURDIR=`pwd`
 
 ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 
-#to describe step Function
 #aws stepfunctions describe-state-machine --state-machine-arn $ARN
 
 
 #ARN=aws stepfunctions describe-step-function --name Kris-StepFunction || jq .arn
 #LizzieTestDev=aws lambda describe-lambda --name $from file || jq .arn
 
-VAR=`cat example.json`
-
+#VAR=`cat example.json`
+VAR='{
+    "StartAt": "LizzieTestDev",
+    "States": {
+        "LizzieTestDev": {
+            "Type": "Task",
+            "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
+            "Next": "LizzieTest"
+        },
+        "LizzieTest": {
+            "Type": "Task",
+            "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
+            "End": true
+        }
+    }
+}'
 
 aws stepfunctions update-state-machine --state-machine-arn $ARN --definition "$VAR" --region $REGION
