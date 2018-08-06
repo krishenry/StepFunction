1dafd8d10b23012b6b685c6488ac34fc697169fe
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-06 12:18:34 +0100

    Definition

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -12,26 +12,50 @@ CURDIR=`pwd`
 ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 
 #aws stepfunctions describe-state-machine --state-machine-arn $ARN
-
-
+declare -a lines
+count=0
+filename='lambdanames.txt'
+chmod +x deployAlias.sh
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	lines[${count}]=line
+	${count}++
+done < $filename
 #ARN=aws stepfunctions describe-step-function --name Kris-StepFunction || jq .arn
 #LizzieTestDev=aws lambda describe-lambda --name $from file || jq .arn
 
 #VAR=`cat example.json`
+# VAR='{
+#     "StartAt": "LizzieTestDev",
+#     "States": {
+#         "LizzieTestDev": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
+#             "Next": "LizzieTest"
+#         },
+#         "LizzieTest": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
+#             "End": true
+#         }
+#     }
+# }'
+
+
 VAR='{
-    "StartAt": "LizzieTestDev",
+    "StartAt": "${( ${lines}[0] )[0]}",
     "States": {
-        "LizzieTestDev": {
+        "${( ${lines}[0] )[0]}": {
             "Type": "Task",
             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
-            "Next": "LizzieTest"
+            "Next": "( ${lines}[1] )[0]"
         },
-        "LizzieTest": {
+        "( ${lines}[1] )[0]": {
             "Type": "Task",
             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
             "End": true
         }
     }
 }'
+
 
 aws stepfunctions update-state-machine --state-machine-arn $ARN --definition "$VAR" --region $REGION
