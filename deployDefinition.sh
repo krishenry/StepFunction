b9e5fc235b7e8c2460c78316e47f160f5c7dcd1c
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-07 14:18:49 +0100

    Step Function IMPROVEMENT 2

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -12,31 +12,12 @@ CURDIR=`pwd`
 ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 
 #aws stepfunctions describe-state-machine --state-machine-arn $ARN
-
 #ARN=aws stepfunctions describe-step-function --name Kris-StepFunction || jq .arn
 #LizzieTestDev=aws lambda describe-lambda --name $from file || jq .arn
 
 VAR=`cat example.json`
-
 echo "$VAR"
 
 REGION="us-east-1"
 
 aws stepfunctions update-state-machine --state-machine-arn $ARN --definition "$VAR" --region $REGION
-
-
-# VAR=`{
-#     "StartAt": "LizzieTestDev12345",
-#     "States": {
-#         "LizzieTestDev12345": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
-#             "Next": "LizzieTest"
-#         },
-#         "LizzieTest": {
-#             "Type": "Task",
-#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
-#             "End": true
-#         }
-#     }
-# }`
