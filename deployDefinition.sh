410c903952ac5fbac1da294f771501e39b60941e
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-07 10:43:04 +0100

    Initialize

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -16,23 +16,24 @@ ARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 #ARN=aws stepfunctions describe-step-function --name Kris-StepFunction || jq .arn
 #LizzieTestDev=aws lambda describe-lambda --name $from file || jq .arn
 
-#VAR=`cat example.json`
+VAR=`cat example.json`
 
-VAR=`{
-    "StartAt": "LizzieTestDev12345",
-    "States": {
-        "LizzieTestDev12345": {
-            "Type": "Task",
-            "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
-            "Next": "LizzieTest"
-        },
-        "LizzieTest": {
-            "Type": "Task",
-            "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
-            "End": true
-        }
-    }
-}`
 #echo "$VAR"
 aws stepfunctions update-state-machine --state-machine-arn $ARN --definition "$VAR" --region $REGION
 
+
+# VAR=`{
+#     "StartAt": "LizzieTestDev12345",
+#     "States": {
+#         "LizzieTestDev12345": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTestDev", 
+#             "Next": "LizzieTest"
+#         },
+#         "LizzieTest": {
+#             "Type": "Task",
+#             "Resource": "arn:aws:lambda:us-east-1:015887481462:function:LizzieTest",
+#             "End": true
+#         }
+#     }
+# }`
