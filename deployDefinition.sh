774b1b015621ce6c703547c9551cd3a67b6cbd75
Ohotski <Kristiyan.Ohotski@thomsonreuters.com>
2018-08-10 12:05:09 +0100

    Step Func Name 2

--- deployDefinition.sh
+++ deployDefinition.sh
@@ -12,7 +12,7 @@ CURDIR=`pwd`
 
 #lambda_ARN="arn:aws:lambda:us-east-1:015887481462:function"
 
-StepARN=$(aws stepfunctions list-state-machines --region $REGION --query 'stateMachines[?name=="'$StepFuncName'"].{stateMachineArn:stateMachineArn}' | jq -r '.[].stateMachineArn')
+StepARN=$(aws stepfunctions list-state-machines --region $REGION --query 'stateMachines[?name=='"$StepFuncName"'].{stateMachineArn:stateMachineArn}' | jq -r '.[].stateMachineArn')
 echo "$StepARN"
 StepARN="arn:aws:states:us-east-1:015887481462:stateMachine:Kris-StepFunction"
 
